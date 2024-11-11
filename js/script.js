8ad950e9f5ce58c378da00f197299bdf5e8f0125
alinamaslina <alinabezuhla040492@gmail.com>
2024-11-11 20:16:16 +0400

    js new codes

---
+++ js/script.js
@@ -0,0 +1,21 @@
+var myVariable=20; myAnotherVariable=30;
+
+console.log ("alina".toUpperCase());
+
+var obj={name:"alina"}
+
+var N = new Number(4000);
+console.log (typeof N);
+var n = 5000;
+console.log (typeof n);
+
+console.log(+10);
+console.log(-10);
+
+console.log(Math.sqrt(25));
+console.log ("Some 'long' string");
+console.log("another \nvery long \nstring");
+var string = "Sometimes the same is different;"
+console.log (string.length);
+console.log (string.charAt(string.length - 2));
+console.log (string.substring(10));
